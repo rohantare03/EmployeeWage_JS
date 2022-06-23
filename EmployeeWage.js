efd3ab447a0c89d3a5104638898d8349012233b9
rohantare03 <rohatare123@gmail.com>
2022-06-23 20:07:48 +0530

    [Rohan] Add : Checking if Employee Present or Absent

---
+++ EmployeeWage.js
@@ -0,0 +1,13 @@
+//UC1: Employee Present or Absent
+{
+    const Is_Present = 1
+    let EmpCheck = Math.floor(Math.random() * 2);
+    if(EmpCheck == Is_Present)
+    {
+        console.log("Employee is Present");
+    }
+    else
+    {
+        console.log("Employee is Absent");
+    }
+}
