2b4c94c582187004c0a5250ce1442412f3ceba20
rohantare03 <rohatare123@gmail.com>
2022-06-23 20:10:57 +0530

    [Rohan] Add : Calculating Daily Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,3 +11,28 @@
         console.log("Employee is Absent");
     }
 }
+
+//UC2: Daily Employee Wage with Switch Case
+const IsPartTime = 1;
+const IsFullTime = 2;
+const PartTimeHrs = 4;
+const FullTimeHrs = 8;
+const WagePerHr = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 3);
+switch (empCheck)
+{
+    case IsPartTime:
+        empHrs = PartTimeHrs;
+        break;
+    case IsFullTime:
+        empHrs = FullTimeHrs;
+        break;
+    default:
+        empHrs = 0;
+        break;
+}
+let empWage = empHrs * WagePerHr;
+console.log("Daily Employee Wage : " + empWage);
+
